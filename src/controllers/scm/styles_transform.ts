2816999fb5a7dcc4a2346e4e19d4561362eea478
dsalazar117 <dsalazar@ai27.com>
2024-06-04 16:35:42 -0600

    scm updated to access files

--- src/controllers/scm/styles_transform.ts
+++ src/controllers/scm/styles_transform.ts
@@ -1,27 +1,54 @@
 // Controller that takes as input the model of StylesAssociation and moves the Styles to the CSS file given by sync
 
-import { StyleAssociation } from "../../models/StyleAssociation";
-
+import * as vscode from 'vscode';
 
 interface ICommand 
 {
     execute() : Promise<string> // Returns the string to be appended at component.scss
 }
 
-export class StylesTransform
+type ClassGrouping = {
+    className: string;
+    cssProperties: string[];
+  };
+  
+export class StylesTransform implements ICommand
 {
-    private sa_model     ?: StyleAssociation[];
-    private thisFile_HTML?: string;
-    private outFile_CSS  ?: string;
+    private class_grouping    : ClassGrouping[];
+    private thisFile_HTML ?: vscode.Uri;
+    private outFile_CSS   ?: vscode.Uri;
 
-    constructor() {}
-
-    public setup(sa_model : StyleAssociation[] , thisFile : string , outFile : string)
+    constructor(class_grouping: ClassGrouping[], thisFile: vscode.Uri | null, outFile: vscode.Uri | null)
     {
-        this.sa_model = sa_model;
-        this.thisFile_HTML = thisFile;
-        this.outFile_CSS = outFile;
+        this.class_grouping = class_grouping;
+        if (thisFile && outFile)
+        {
+            this.thisFile_HTML = thisFile;
+            this.outFile_CSS = outFile;
+            console.log({
+                class_grouping: this.class_grouping,
+                thisFile_HTML: this.thisFile_HTML,
+                outFile_CSS: this.outFile_CSS
+            });
+        }
     }
 
-
+    execute(): Promise<string> 
+    {
+        return new Promise((resolve, reject) => 
+        {
+            if (!this.class_grouping || !this.outFile_CSS) 
+            {
+                return reject('Class grouping or output file not set up correctly.');
+            }
+            vscode.workspace.openTextDocument(this.outFile_CSS).then(document => 
+            {
+                resolve(document.getText());
+            }, err => 
+            {
+                reject(`Failed to open text document: ${err}`);
+            });
+        });
+    }
+    
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode';
-
+import * as path from 'path';
 import { showToast } from './views/toast_test';
 import { DataHelper } from './controllers/db/dataHelper';
 import { Secrets } from './Secrets';
 
 import { StyleParserCommand } from './controllers/parsing/extract_styles';
 import { StyleAssociation } from './models/StyleAssociation';
+import { StylesTransform } from './controllers/scm/styles_transform';
 
 
 export function activate(context: vscode.ExtensionContext) 
@@ -17,14 +18,84 @@ export function activate(context: vscode.ExtensionContext)
     let command1 = vscode.commands.registerCommand('extension.showToast', () => 
 	{
         const sp : StyleParserCommand = new StyleParserCommand();
-        
         sp.execute()
         .then((res : StyleAssociation[]) => 
         {
-            showToast(JSON.stringify(res,null,2));
-            console.log(JSON.stringify(res,null,2));
+            
+            const all_elements : any = {};
+            res.forEach((record) => {
+                all_elements[record.element] = [];
+                record.tags.forEach((style) => {
+                    all_elements[record.element].push(`${style.key}:${style.value}`);
+                });
+            });
+            const invertedDictionary: { [key: string]: string[] } = {};
+            for (const tagPosition in all_elements) {
+                all_elements[tagPosition].forEach((style : any) => {
+                    if (!invertedDictionary[style]) {
+                        invertedDictionary[style] = [];
+                    }
+                    invertedDictionary[style].push(tagPosition);
+                });
+            }
+    
+            // Algoritmo desarrollado en Python style_associations.ipynb
+            const groupedProperties: { [key: string]: string[] } = {};
+            for (const cssProperty in invertedDictionary) {
+                invertedDictionary[cssProperty].forEach((element) => {
+                    if (!groupedProperties[element]) {
+                        groupedProperties[element] = [];
+                    }
+                    groupedProperties[element].push(cssProperty);
+                });
+            }
+            const cssClasses: { [key: string]: string[] } = {};
+            Object.keys(groupedProperties).forEach((element, index) => {
+                const className = `class-${index + 1}`;
+                cssClasses[className] = groupedProperties[element];
+            });
+
+            const propertyToClass: { [key: string]: string[] } = {};
+            for (const className in cssClasses) {
+                const properties = cssClasses[className].sort().join(';');
+                if (!propertyToClass[properties]) {
+                    propertyToClass[properties] = [];
+                }
+                propertyToClass[properties].push(className);
+            }
+
+            const mergedClasses: { [key: string]: string[] } = {};
+            for (const properties in propertyToClass) { 
+                const classNames = propertyToClass[properties];
+                if (classNames.length > 1) {
+                    mergedClasses[classNames[0]] = properties.split(';');
+                } else {
+                    mergedClasses[classNames[0]] = properties.split(';');
+                }
+            }
+            const mergedClassesArray = Object.entries(mergedClasses).map(([className, cssProperties]) => ({ className, cssProperties }));
+            // Move this object to the the scm object as a parameter 
+            
+            //This just prints
+            
+            const currentDocumentUri = vscode.window.activeTextEditor?.document.uri || null;
+            const currentFolderUri = currentDocumentUri ? vscode.Uri.file(path.dirname(currentDocumentUri.fsPath)) : null;
+            
+            if (currentFolderUri) 
+            {
+                vscode.workspace.findFiles(new vscode.RelativePattern(currentFolderUri, '*.{css,scss}'), undefined, 100)
+                    .then(cssFiles => {
+                        if (cssFiles.length > 0) 
+                        {
+                            
+                            const cssFileName = cssFiles[0];
+                            const scm = new StylesTransform(mergedClassesArray , currentDocumentUri , cssFileName);
+                            scm.execute().then((res) => {console.log(res);});
+                        }
+                    });
+            }
         });
-        console.log("Command run");
+
 
     });
     
